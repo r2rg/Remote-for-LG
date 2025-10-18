bcd017768c20c5066cbcbce94c36e61e0f751ef3
Arthur <arturik2004@yandex.ru>
2025-10-18 21:23:13 +0300

    .

--- Remote for LG/Service/ConnectionService.swift
+++ Remote for LG/Service/ConnectionService.swift
@@ -9,9 +9,9 @@ fileprivate enum Constants {
 final class ConnectionService: WebOSClientDelegate {
     let tvManager: TVManager
 
-    var client: WebOSClient? // <--- Changed from 'private' to internal (default)
+    var client: WebOSClient?
     private(set) var isPromptingForPin = false
-    var enteredPin: String = "" // Bind this to your TextField in SwiftUI
+    var enteredPin: String = ""
 
     init(tvManager: TVManager) {
         self.tvManager = tvManager
@@ -25,41 +25,31 @@ final class ConnectionService: WebOSClientDelegate {
             return
         }
 
-        // Hold onto the client and set delegate
         let client = WebOSClient(url: url, delegate: self, shouldLogActivity: true)
         self.client = client
         client.connect()
 
-        // On initial connection, use clientKey if available
         let registrationToken = UserDefaults.standard.string(forKey: Constants.registrationTokenKey)
         client.send(.register(pairingType: .pin, clientKey: registrationToken))
     }
 
-    // Call this from your UI when the user submits the PIN
     func submitPin() {
         guard let client = client, !enteredPin.isEmpty else { return }
         client.send(.setPin(enteredPin))
-        // Hide the PIN entry UI until we know if it succeeded
         isPromptingForPin = false
     }
 
-    // MARK: - WebOSClientDelegate
 
     func didDisplayPin() {
-        // Called when the TV shows a pin (user should enter it)
         isPromptingForPin = true
         enteredPin = ""
     }
 
     func didRegister(with clientKey: String) {
-        // Successfully paired, store the key
         UserDefaults.standard.set(clientKey, forKey: Constants.registrationTokenKey)
     }
 
     func didReceiveNetworkError(_ error: Error?) {
-        // Handle disconnect/retry as needed
         isPromptingForPin = false
     }
-
-    // Optional: implement other delegate methods as needed
 }
